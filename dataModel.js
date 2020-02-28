aa69064dfa00daf052c2351195a13b56ba88f599
Valdo99 <edvaldogjonikaj@MacBook-Pro-di-Edvaldo.local>
2020-02-28 19:57:44 +0100

    added server

---
+++ dataModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+let Articolo = new Schema({
+    Data: {
+        type: Date
+    },
+    Titolo: {
+        type: String
+    },
+    Testo: {
+        type:String
+    },
+    Immagine:{
+        type: String
+    }
+
+});
+module.exports = mongoose.model('Articolo', Articolo);
---
+++ server.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const gioRoutes = express.Router();
+const PORT = 4000;
+let Articolo = require('./dataModel.js');
+app.use(cors());
+app.use(bodyParser.json());
+mongoose.connect('mongodb://127.0.0.1:27017/Blog', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
+const connection = mongoose.connection;
+connection.once('open', function () {
+    console.log("MongoDB database connection established successfully");
+})
+gioRoutes.route('/').get(function (req, res) {
+    Articolo.find(function (err, articolo) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(articolo);
+        }
+    });
+});
+gioRoutes.route('/:id').get(function (req, res) {
+    let id = req.params.id;
+    Articolo.findById(id, function (err, articolo) {
+        res.json(articolo);
+    });
+});
+gioRoutes.route('/update/:id').post(function (req, res) {
+    Articolo.findById(req.params.id, function (err, art) {
+        if (!art)
+            res.status(404).send("data is not found");
+        else
+        art.Data = req.body.Data;
+        art.Titolo = req.body.Titolo;
+        art.Testo = req.body.Testo;
+        art.Immagine = req.body.Immagine;
+        art.save().then(articolo => {
+                res.json('articol updated!');
+            })
+            .catch(err => {
+                res.status(400).send("Update not possible");
+            });
+    });
+});
+gioRoutes.route('/add').post(function (req, res) {
+    let articolo = new Articolo(req.body);
+    articolo.save()
+        .then(q => {
+            res.status(200).json({
+                'song': 'article added successfully'
+            });
+        })
+        .catch(err => {
+            res.status(400).send('adding new artcile failed');
+        });
+});
+// songRoutes.route('/add/bulk').post(function (req, res) {
+//     Song.insertMany(req.body, function (error, docs) {
+//         if (error === null ) {
+//             res.status(200).send('bulk methond worked correctly: ' + docs)
+//         } else {
+//             res.status(400).send('error on bulk method: ' + error)
+//         }
+//     });
+// });
+
+gioRoutes.route('/delete/:id').delete((req, res, next) => {
+    Articolo.findByIdAndRemove(req.params.id, (error, data) => {
+        if (error) {
+            res.status(400).json({
+                "error": next(error)
+            })
+        } else {
+            res.status(200).json({
+                "msg": data
+            })
+        }
+    })
+});
+
+app.use('/Blog', gioRoutes);
+app.listen(PORT, function () {
+    console.log("Server is running on Port: " + PORT);
+});
