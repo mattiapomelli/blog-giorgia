6929228e26db8cd7f5ba2aa4fc69be12035d85e0
Mattia <mattiapomelli@gmail.com>
2020-02-29 10:20:25 +0100

    Now displaying articles

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,45 @@
 import React, {Component} from 'react'
 
 export default class Home extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            articles: []
+        }
+
+        this.listArticles = this.listArticles.bind(this)
+    }
+
+    componentDidMount() {
+        console.log("component did mount")
+        this.listArticles()
+    }
+
+    listArticles() {
+        fetch('http://localhost:4000/Blog')
+            .then(res => res.json())
+            .then(data => {
+                console.log("DATA", data)
+                this.setState({articles: data})
+                console.log("STATE", this.state)
+            })
+            
+    }
+
     render() {
         return (
         <div>
-            Here the list of articles with pagination!
+            {
+                this.state.articles.map(article => {
+                    return (
+                        <div key={article._id}>
+                            <h1>{article.Titolo}</h1>
+                            <h2>{article.Testo}</h2>
+                        </div>
+                    )
+                })
+            }
         </div>)
 
     }
